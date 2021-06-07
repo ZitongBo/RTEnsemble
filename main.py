62c7a440b0987b413da84cd58722ad9adaa0fbfe
ZitongBo <540306311@qq,cin>
2021-06-07 15:13:37 +0800

    train ensemble

--- main.py
+++ main.py
@@ -1,20 +1,10 @@
 # This is a sample Python script.
-
+import classifier
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-    import numpy as np
-
-    tmp = np.array([1, 3, 0, 8])
-    print(np.where(tmp == 0))
-    tmp = np.delete(tmp, np.where(tmp == 0), 0)
-    print(np.sum(tmp)/len(tmp))
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    classifier.train('audio','dt',0)
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
